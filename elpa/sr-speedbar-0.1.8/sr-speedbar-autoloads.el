f0635747881af81b6104189b3a62484f936bc634
yods <yodit_berga@yahoo.co.uk>
2013-10-06 21:03:38 +0100

    emacs live

---
+++ elpa/sr-speedbar-0.1.8/sr-speedbar-autoloads.el
@@ -0,0 +1,19 @@
+;;; sr-speedbar-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "sr-speedbar" "sr-speedbar.el" (21057 22699
+;;;;;;  281765 892000))
+;;; Generated autoloads from sr-speedbar.el
+
+(provide 'sr-speedbar)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; sr-speedbar-autoloads.el ends here
